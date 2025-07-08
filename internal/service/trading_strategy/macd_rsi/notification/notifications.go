2924f0b8c8c9863b29876f66e5d170192a9ca916
Ваше Имя <you@example.com>
2025-07-08 23:21:05 +0300

    add strategy macd rsi

---
+++ internal/service/trading_strategy/macd_rsi/notification/notifications.go
@@ -0,0 +1,26 @@
+package notification
+
+import (
+	"strings"
+	"tinvest/internal/domain/atr"
+	"tinvest/internal/model"
+)
+
+func Trade(shares []model.Share, atrs map[string]atr.ItemTechAnalyse) string {
+	notifyMessageBuilder := strings.Builder{}
+	notifyMessageBuilder.WriteString("🟢 \n<u><b>К ПОКУПКЕ (Super Trend):</b></u>\n\n\n<code>")
+
+	for _, share := range shares {
+		notifyMessageBuilder.WriteString("<u>" + share.Name + "</u>")
+
+		if atr, exist := atrs[share.ID]; exist {
+			notifyMessageBuilder.WriteString(" <u>" + atr.ToString() + "</u>")
+		}
+
+		notifyMessageBuilder.WriteString("\n")
+	}
+
+	notifyMessageBuilder.WriteString("</code>")
+
+	return notifyMessageBuilder.String()
+}
