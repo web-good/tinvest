132679a5439074f85a67d7d93c7bb9806929af44
Ваше Имя <you@example.com>
2026-06-03 00:32:17 +0300

    edit

--- internal/app/app.go
+++ internal/app/app.go
@@ -13,7 +13,6 @@ import (
 	gxmodel "tinvest/internal/service/trading_strategy/golden_x/model"
 	"tinvest/internal/service/trading_strategy/golden_x/scheduler"
 	scalpingdto "tinvest/internal/service/trading_strategy/scalping/dto"
-	scalpingscheduler "tinvest/internal/service/trading_strategy/scalping/scheduler"
 	"tinvest/internal/service_provider"
 	"tinvest/pkg/closer"
 	"tinvest/pkg/logger"
@@ -230,7 +229,7 @@ func (a *App) runProd(ctx context.Context) {
 	/*go func() {
 	defer wg.Done()
 	sh := mr.NewSchedulerService(a.sp.GetMacdRsiTradingService())
-	err := sh.TakeProfit(ctx, dto.TakeProfit{Interval: enum.Hour1, ATRInterval: enum.Day1, Scheduler: "*/ //2 8-23 * * *"})
+	err := sh.TakeProfit(ctx, dto.TakeProfit{Interval: enum.Hour1, ATRInterval: enum.Day1, Scheduler: "*///2 8-23 * * *"})
 
 	/*if err != nil {
 			logger.ErrorContext(ctx, "Error in worker macd rsi 1H take profit", err.Error())
@@ -299,7 +298,7 @@ func (a *App) runProd(ctx context.Context) {
 			logger.ErrorContext(ctx, "Error in worker golden X strategy ShareTip:2", err.Error())
 		}
 	}()
-	go func() {
+	/*go func() {
 		defer wg.Done()
 		err := scalpingscheduler.NewSchedulerService(a.sp.GetScalpingTradingService()).Trade(
 			ctx,
@@ -310,6 +309,6 @@ func (a *App) runProd(ctx context.Context) {
 		if err != nil {
 			logger.ErrorContext(ctx, "Error in worker Scalping", err.Error())
 		}
-	}()
+	}()*/
 	wg.Wait()
 }
